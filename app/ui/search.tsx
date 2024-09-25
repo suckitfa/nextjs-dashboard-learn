d9d74d0ac30238839ce9ff15962c662a4f1c5503
GuangyuanTang <254202042@qq.com>
2024-09-25 23:51:13 +0800

    feat: search input

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,14 +1,31 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams()
+  const pathname = usePathname();
+  const { replace } = useRouter();
+  const handleInputChange = (inputVal: string) => {
+    const params = new URLSearchParams(searchParams);
+    console.log('handleInputChange = ', inputVal)
+    if (inputVal) {
+      params.set('query', inputVal);
+    } else {
+      params.delete('query');
+    }
+    const queryURL = `${pathname}?${params.toString()}`
+    console.log('queryURL ===>', queryURL)
+    replace(queryURL);
+  }
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
         Search
       </label>
       <input
+        defaultValue={searchParams.get('query')?.toString()}
+        onChange={e => handleInputChange(e.target.value)}
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
       />
